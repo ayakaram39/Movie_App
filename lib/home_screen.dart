5a8904aa8425f41110026409ae03d976245dde48
Aya karam <ayakaram13545@gmail.com>
2026-08-25 18:11:01 +0200

    splash+home done

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,9 +6,56 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body:
-         Center(),
+      backgroundColor: const Color(0xFF242A32),
 
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF242A32),
+        elevation: 0,
+        title: const Text(
+          'What do you want to watch?',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Search',
+                hintStyle: const TextStyle(
+                  color: Colors.grey,
+                ),
+                suffixIcon: const Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+                filled: true,
+                fillColor: const Color(0xFF343A40),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(
+                    color: Colors.orange,
+                    width: 2,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(
+                    color: Colors.orange,
+                    width: 2,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomeScreen(),
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff242A32),
-      body:
-          Center(
-            child: Image.asset('assets/splash.png'),
-          )
-
+      backgroundColor: const Color(0xFF242A32),
+      body: Center(
+        child: Image.asset('assets/splash.png'),
+      ),
     );
   }
 }
